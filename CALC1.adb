44cd702121cb318e332a4b5ad756d7de7f9f575f
Randal-Robin <40613540+Randal-Robin@users.noreply.github.com>
2018-09-25 13:19:01 -0500

    Updated Code

--- CALC1.adb
+++ CALC1.adb
@@ -7,21 +7,12 @@
 -- Functions include adding to a, taking the square root of c, and raising c to a power
 -- Aditional functions are printing c and stopping the program
 
-with Ada.Text_IO, Ada.Integer_Text_IO;
-use Ada.Text_IO, Ada.Integer_Text_IO;
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Float_Text_IO;
+
 procedure CALC1 is
-begin
-   -- Request user input
-   Put_Line("READY FOR INPUT");
-   -- Get variable name for load
-   Get (load);
-   -- Request user input value for load
-   Put_Line("ENTER VALUE FOR" load);
-   -- Get value for load
-   Get (load);
-   Get (mem);
-   Put_Line("VALUE OF" mem "IS" c);
-end CALC1;
+
+mem : array(1 .. 300) of String(1 .. 100); --array used for the name of the variable
+load : array (1 .. 300) of float;
 
 -- Create object, load, which will accept variable name and value
 procedure load is
@@ -36,7 +27,7 @@ procedure mem is
 end mem;
 
 -- Create add function
-procedure + is
+function add (floa, flo) is
    mem val = load + load;
 end +;
 
@@ -54,3 +45,16 @@ end ^;
 procedure print is
    Put_Line();
 end print;
+
+begin
+   -- Request user input
+   Ada.Text_IO.Put_Line("READY FOR INPUT");
+   -- Get variable name for load
+   Ada.Text_IO.Get (load);
+   -- Request user input value for load
+   Ada.Integer_Text_IO.Put_Line("ENTER VALUE FOR" load);
+   -- Get value for load
+   Ada.Text_IO.Get (load);
+   Ada.Text_IO.Get (mem);
+   Ada.Text_IO.Put_Line("VALUE OF" mem "IS" c);
+end CALC1;
